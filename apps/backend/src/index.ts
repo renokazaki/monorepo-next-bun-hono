c321daa055089b781ecabd8505e1124bff562482
ren <ren84690@gmail.com>
2025-04-16 10:53:32 +0900

    first commit

---
+++ apps/backend/src/index.ts
@@ -0,0 +1,17 @@
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+
+const app = new Hono();
+
+app.use(
+  "*",
+  cors({
+    origin: "*",
+  })
+);
+const route = app.get("/hello", (c) => {
+  return c.json({ message: "Hello Hono!" });
+});
+export type AppType = typeof route;
+
+export default app;
